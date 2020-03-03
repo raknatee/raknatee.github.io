a40c15f179b83fd23e8f5b7c6e4086a05172c596
Raknatee <raknatee@gmail.com>
2020-03-03 21:56:17 +0700

    update

---
+++ precache-manifest.436a8582724521b2c2c42ef19e430c7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d5e8430d32af14aa33",
+    "url": "/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "98ae6d262a377477c0975709978c4069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3fa33638bb0784dc1c",
+    "url": "/js/about.486ee840.js"
+  },
+  {
+    "revision": "c7d5e8430d32af14aa33",
+    "url": "/js/app.62d56011.js"
+  },
+  {
+    "revision": "582a9b97c7d5ae98a5ec",
+    "url": "/js/chunk-vendors.1ab9f8ed.js"
+  },
+  {
+    "revision": "e6434533477b589dbaaee964abee527c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
